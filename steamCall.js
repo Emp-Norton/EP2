e1234c093edd8c08442b1f8530d5a3308d843056
Emp-Norton <jamesjelenko@gmail.com>
2017-11-16 11:27:17 -0800

    Added a few more relevantFields.

--- steamCall.js
+++ steamCall.js
@@ -3,7 +3,7 @@ var loadXML = function(){
   var id = prompt("Please enter user Steam ID");
   var url = 'http://localhost:4000/steam/' + id;
   var xhttp = new XMLHttpRequest();
-  var relevantFields = ["personaname"];
+  var relevantFields = ["personaname", "lastlogoff", "profileurl"];
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4) {
         var response = JSON.parse(this.response).response.players[0]
@@ -12,6 +12,8 @@ var loadXML = function(){
           if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
         });
        $('#results').html(`<p>${JSON.stringify(relevantDetails)}</p>`);
+       $('#profileViewer').html(`<object type="text/html" data="${relevantDetails.profileurl}" width="400px" height="300px" style="overflow:auto;border:5px ridge blue">
+    </object>`)
       }
   };
   
