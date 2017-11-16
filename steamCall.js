44bcf9f7aff93bcaaa289038d6499f6cfd6f26cc
Emp-Norton <jamesjelenko@gmail.com>
2017-11-16 12:28:56 -0800

    Revamped to match new Index / CSS.

--- steamCall.js
+++ steamCall.js
@@ -1,25 +1,30 @@
 
 var loadXML = function(){
-  var id = prompt("Please enter user Steam ID");
+  var id = '76561198254047403' // prompt("Please enter user Steam ID");
   var url = 'http://localhost:4000/steam/' + id;
   var xhttp = new XMLHttpRequest();
   var relevantFields = ["personaname", "lastlogoff", "profileurl"];
+  var relevantDetails = {};  
   xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
+      if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(this.response).response.players[0]
-        var relevantDetails = {};
+        
         Object.keys(response).forEach(function(key){ 
           if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
         });
-       $('#results').html(`<p>${JSON.stringify(relevantDetails)}</p>`);
-       $('#profileViewer').html(`<object type="text/html" data="${relevantDetails.profileurl}" width="400px" height="300px" style="overflow:auto;border:5px ridge blue">
-    </object>`)
       }
+      for (var detail in relevantDetails){
+        $('#playerInfo').append(`<p>${JSON.stringify(relevantDetails[detail])}</p>`);
+      };
+      $('#profileViewer').html(`<object type="text/html" id="profileObject" data="${relevantDetails.profileurl}"></object>`);
+      $('#playerName').html(`<p>${relevantDetails.personaname}<p>`);
   };
   
   xhttp.open("GET", url, true);
   xhttp.send();
 }
+
+
 
 
 
