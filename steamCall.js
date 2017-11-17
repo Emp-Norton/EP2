cde6a1432efe3b584e22ff44ad1c96bc37f4fe78
Emp-Norton <jamesjelenko@gmail.com>
2017-11-17 15:12:37 -0800

    Messed around with the logic for loadXML, having problem converting gameList (with appids) to gameNames (with string names) and populating. Mutual recursion between loadXML / renderResults seems to be culprit.

--- steamCall.js
+++ steamCall.js
@@ -1,41 +1,62 @@
 
+var relevantFields = ["personaname", "lastlogoff", "profileurl"];
+var relevantDetails = {};
+var friendList = [];
+var gamesList = [];
+var gameNames = [];
+
 var loadXML = function(id, type){
-  //'76561198254047403'
   var url = `http://localhost:4000/${type}/${id}`;
   var xhttp = new XMLHttpRequest();
-  var relevantFields = ["personaname", "lastlogoff", "profileurl"];
-  var relevantDetails = {};  
   xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        if (type === "friends") console.log(this.response)
-        var response = JSON.parse(this.response).response.players[0]
+    if (this.readyState == 4 && this.status == 200) {
+      if (type === "friends"){
+        friendList = JSON.parse(this.response).friendslist.friends;
+        for (var friendIndex in friendList){
+          $('#playerFriends').prepend(`<p>${JSON.stringify(friendList[friendIndex].steamid)}</p>`) // move these to more appropriate place
+        };
+      } else if (type === "games"){
+        gamesList = JSON.parse(this.response).response.games;
+        getGameNames(gamesList);
+        if (gameNames.length > 0) renderResults();
+      } else if (type === "profile") {
+        var response = JSON.parse(this.response).response.players[0];
         Object.keys(response).forEach(function(key){ 
-         if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
+          if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
         });
+        getFriends(id);
+        getGames(id);
+      } else {
+        var response = JSON.parse(this.response)[`${id}`]
+        gameNames.push(response.data.name);
       }
-      // these next lines don't belong here, move them to their own function -- inside render seems best for now.
-      for (var detail in relevantDetails){
-        $('#playerInfo').append(`<p>${JSON.stringify(relevantDetails[detail])}</p>`);
-      };
-      $('#profileViewer').html(`<object type="text/html" id="profileObject" data="${relevantDetails.profileurl}"></object>`);
-      $('#playerName').html(`<p>${relevantDetails.personaname}<p>`);
+    }
   };
-  
   xhttp.open("GET", url, true);
   xhttp.send();
 }
 
 var renderResults = function(){
-  loadXML($('#playerSearchField').val(), "friends") // call call with "friends" instead, but need to instantiate a storage div or player object for results.
-  $('#playerInfo').show();
-  $('#profileViewer').show();
-  $('#playerSearch').hide();
+    for (var detail in relevantDetails){
+      $('#playerInfo').prepend(`<p>${JSON.stringify(relevantDetails[detail])}</p>`);
+    };
+    $('#profileViewer').html(`<object type="text/html" id="profileObject" data="${relevantDetails.profileurl}"></object>`);
+    $('#playerName').html(`<p>${relevantDetails.personaname}<p>`);
+    $('#playerInfo').show();
+    $('#profileViewer').show();
+    $('#playerSearch').hide();
 }
 // these should be prototype methods on the player object.
-var getFriends = function(){
-  loadXML($('#playerSearchField').val(), "friends")
+var getFriends = function(id){
+  loadXML(id, "friends");
 }
 
+var getGames = function(id){
+  loadXML(id, "games");
+}
 
-
-
+var getGameNames = function(gameList){
+  for (var gameIdx = 0; gameIdx < gameList.length; gameIdx++){
+    loadXML(gameList[gameIdx].appid, "gameDetails")
+  }
+}
