1fbfbb478b6fc1a7226da956c69c94a06bd667c3
Emp-Norton <jamesjelenko@gmail.com>
2017-11-17 12:19:47 -0800

    Reworking loadXML / renderResults functions to be more self-encapsulated.

--- steamCall.js
+++ steamCall.js
@@ -1,18 +1,19 @@
 
-var loadXML = function(userId){
-  var id = userId; //'76561198254047403'
-  var url = 'http://localhost:4000/steam/' + id;
+var loadXML = function(id, type){
+  //'76561198254047403'
+  var url = `http://localhost:4000/${type}/${id}`;
   var xhttp = new XMLHttpRequest();
   var relevantFields = ["personaname", "lastlogoff", "profileurl"];
   var relevantDetails = {};  
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        if (type === "friends") console.log(this.response)
         var response = JSON.parse(this.response).response.players[0]
-        
         Object.keys(response).forEach(function(key){ 
-          if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
+         if (relevantFields.includes(key)) relevantDetails[`${key}`] = response[`${key}`];
         });
       }
+      // these next lines don't belong here, move them to their own function -- inside render seems best for now.
       for (var detail in relevantDetails){
         $('#playerInfo').append(`<p>${JSON.stringify(relevantDetails[detail])}</p>`);
       };
@@ -25,12 +26,15 @@ var loadXML = function(userId){
 }
 
 var renderResults = function(){
-  loadXML($('#playerSearchField').val());
+  loadXML($('#playerSearchField').val(), "friends") // call call with "friends" instead, but need to instantiate a storage div or player object for results.
   $('#playerInfo').show();
   $('#profileViewer').show();
   $('#playerSearch').hide();
 }
-
+// these should be prototype methods on the player object.
+var getFriends = function(){
+  loadXML($('#playerSearchField').val(), "friends")
+}
 
 
 
