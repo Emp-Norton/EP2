42a5769a8db9c14e401ec1dd7ab0d5f25553f5bb
Emp-Norton <jamesjelenko@gmail.com>
2017-11-17 12:18:59 -0800

    added playerClass.js script for future work.

--- playerClass.js
+++ playerClass.js
@@ -1,11 +1,18 @@
 var Player = function(name){
-	var player = {
+	var player = Object.create(Player.prototype)
+    player = {
 		name: "",
+		steamId: "",
+		uPlayId: "",
 		games: [],
 		personality: "",
 		friends: []
 	}
 	player.name = name;
 	return player
+}
+
+Player.prototype.loadXML = function(type){
+	loadXML(this.steamId, type)
 }
 
