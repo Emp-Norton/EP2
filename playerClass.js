77dd974fb04cfa419c6f75a8963cf0d4cc5d3e78
James Jelenko <jamesjelenko@gmail.com>
2017-11-20 10:34:40 -0800

    Broadening criteria for matching players.

--- playerClass.js
+++ playerClass.js
@@ -2,17 +2,32 @@ var Player = function(name){
 	var player = Object.create(Player.prototype)
     player = {
 		name: "",
+		uname: "",
 		steamId: "",
 		uPlayId: "",
-		games: [],
-		personality: "",
-		friends: []
+		games: {
+			steam: [],
+			uplay: [],
+			android: [],
+			xbox: [],
+			playstation: [],
+		},
+		personality: [],
+		friends: [],
+		peakHrs: [],
+		typicalDuration: undefined,
+		noMods: undefined,
+		attrWeights: {
+			availability: undefined,
+			personality: undefined,
+			playformOverlap: undefined,
+			gameOverlap: undefined,
+			duration: undefined,
+			mods: undefined
+		}
 	}
 	player.name = name;
 	return player
 }
 
-Player.prototype.loadXML = function(type){
-	loadXML(this.steamId, type)
-}
 
